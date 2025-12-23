20edb18050f651aa6146ac5d57e06d6d0e745a09
marco76tv <marco.sottana@gmail.com>
2025-12-23 12:23:49 +0100

    .

--- app/Models/BaseTreeModel.php
+++ app/Models/BaseTreeModel.php
@@ -6,15 +6,15 @@ namespace Modules\Limesurvey\Models;
 
 // use Laravel\Scout\Searchable;
 use Modules\Xot\Contracts\HasRecursiveRelationshipsContract;
-use Modules\Xot\Models\Traits\TypedHasRecursiveRelationships;
+use Staudenmeir\LaravelAdjacencyList\Eloquent\HasRecursiveRelationships;
 
 /**
  * Base Tree Model for Limesurvey module.
  *
- * Provides recursive relationships using TypedHasRecursiveRelationships trait
- * which wraps the vendor HasRecursiveRelationships with proper type hints.
+ * Provides recursive relationships using HasRecursiveRelationships trait
+ * from staudenmeir/laravel-adjacency-list package.
  */
 abstract class BaseTreeModel extends BaseModel implements HasRecursiveRelationshipsContract
 {
-    use TypedHasRecursiveRelationships;
+    use HasRecursiveRelationships;
 }
